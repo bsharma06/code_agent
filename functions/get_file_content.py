2c4bdaf98bf9957c79734a805a8296aac8170756
Bhoopesh Sharma <bhupeshsharma06@gmail.com>
2025-09-13 15:14:45 +0530

    Initial commit

---
+++ functions/get_file_content.py
@@ -0,0 +1,22 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_filepath = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_filepath.startswith(abs_working_dir):
+        return f"Error: {file_path} is not in the working dir"
+    if not os.path.isfile(abs_filepath):
+        return f"Error: {file_path} is not a file"
+    
+    file_content_string = ""
+    try:
+        with open(abs_filepath, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if len(file_content_string) >= MAX_CHARS:
+                file_content_string += (
+                    f"[...File {file_path} truncated at 10000 characters]"
+                )
+        return file_content_string
+    except Exception as e:
+        return f"Eception reading file: {e.args}"
--- main.py
+++ main.py
@@ -1,19 +1,49 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 load_dotenv()
-
-
 
 def main():
     gemini_api_key = os.getenv("GEMINI_API_KEY")
     client = genai.Client(api_key=gemini_api_key)
+    
+    if len(sys.argv) < 2:
+        print("I need prompt!")
+        sys.exit(1)
+
+    verbose_flag = False
+    if len(sys.argv) == 3 and sys.argv[2]=="--verbose":
+        verbose_flag = True
+    
+    prompt = sys.argv[1]
+    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
+
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents="Why is the sky blue?"
+        model="gemini-2.0-flash-001", contents = messages
     )
+    
     print(response.text)
+    
+    if response.usage_metadata == None:
+        print("There is some problem with LLM response!")
+        return None
+    
+    if verbose_flag:
+        print(f"Input tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Output tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    
+    # print(get_files_info("calculator", "../"))
+
+    print(get_file_content("calculator","pkg/calculator.py"))
