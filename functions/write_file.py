b0180f3a730a7359db82843e64be79c5d33db661
Bhoopesh Sharma <bhupeshsharma06@gmail.com>
2025-09-14 13:28:26 +0530

    Final working agent

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,23 +1,6 @@
 import os
 from google.genai import types
 
-schema_write_file = types.FunctionDeclaration(
-    name="write_file",
-    description="Overwrites an existing file or writes to a new file if ti doesn't exist (and creats required parent dirs safely), constrained to the working directory.",
-    parameters=types.Schema(
-        type=types.Type.OBJECT,
-        properties={
-            "file_path": types.Schema(
-                type=types.Type.STRING,
-                description="The path to the file to write.",
-            ),
-            "contents": types.Schema(
-                type=types.Type.STRING,
-                description="The contents to write to the file as a string.",
-            ),
-        },
-    ),
-)
 
 def write_file(working_directory, file_path, content):
     abs_working_dir = os.path.abspath(working_directory)
@@ -47,3 +30,21 @@ def write_file(working_directory, file_path, content):
         return f"Successfully wrote to '{file_path}' ({len(content)} characters)"
     except Exception as e:
         return f"Failed to write to file: {file_path}, {e.args}"
+    
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Overwrites an existing file or writes to a new file if ti doesn't exist (and creats required parent dirs safely), constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to write.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to write to the file as a string.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from functions.get_files_info import schema_get_files_info
 from functions.get_file_content import schema_get_file_content
 from functions.write_file import schema_write_file
 from functions.run_python_file import schema_run_python_file
+from call_function import call_function
+from config import MAX_ITERS
 
 load_dotenv()
 
@@ -24,7 +26,14 @@ def main():
         - Write to a file (create or update)
         - Run a Python file with optional arguments
 
-        All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+        When the user asks about the code project - they are referring to the
+        working directory. So, you should typically start by looking at the
+        projects's files, and figuring out how to run the project and how to
+        run it's tests. You'll always want to test the tests and the actual project
+        to verify that behavior is working.
+
+        All paths you provide should be relative to the working directory.
+        You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
     """
 
     if len(sys.argv) < 2:
@@ -54,25 +63,42 @@ def main():
         tools=[available_function], system_instruction=system_prompt
     )
 
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents = messages,
-        config= config
-    )
-    
-    if response.usage_metadata == None:
-        print("There is some problem with LLM response!")
-        return None
-    
-    if verbose_flag:
-        print(f"Input tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Output tokens: {response.usage_metadata.candidates_token_count}")
+    for i in range(0, MAX_ITERS):
 
-    if response.function_calls:
-        # print(response.function_calls)
-        for function_call_part in response.function_calls:
-            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-    else:
-        print(response.text)
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents = messages,
+            config= config
+        )
+        
+        if response is None or response.usage_metadata == None:
+            print("There is some problem with LLM response!")
+            return None
+        
+        if verbose_flag:
+            print(f"User prompt: {prompt}")
+            print(f"Input tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Output tokens: {response.usage_metadata.candidates_token_count}")
+
+        if response.candidates:
+            for candidate in response.candidates:
+                if candidate is None or candidate.content is None:
+                    continue
+                messages.append(candidate.content)
+
+        if response.function_calls:
+            # print(response.function_calls)
+            for function_call_part in response.function_calls:
+                # print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+                result = call_function(function_call_part, verbose_flag)
+                # print(result)
+                messages.append(
+                    result
+                )
+        else:
+            # Final agent text message
+            print(response.text)
+            return 
 
 if __name__ == "__main__":
     main()
